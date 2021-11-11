c80abf410a8dda86ed5951cd867d8fd61f354d8e
bonishthapa <bonishthapa@gmail.com>
2021-11-11 21:29:36 +0545

    staticfiles

--- consult/settings.py
+++ consult/settings.py
@@ -157,6 +157,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
