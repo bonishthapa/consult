1888f43c82d5fdce31ba0458e51331b30604e294
bonishthapa <bonishthapa@gmail.com>
2021-11-12 23:04:26 +0545

    pp datab

--- consult/settings.py
+++ consult/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
