f0344f2dd06285b4da7fa37ea35853b65b68b610
bonishthapa <bonishthapa@gmail.com>
2021-11-17 19:36:16 +0545

    couting m

--- student/models.py
+++ student/models.py
@@ -72,7 +72,7 @@ class Student(models.Model):
     def __str__(self):
         return self.name
 
-    class Meta:
+    # class Meta:
         # ordering = ['-created_at']    
 
 @receiver(models.signals.post_delete, sender=Student)
